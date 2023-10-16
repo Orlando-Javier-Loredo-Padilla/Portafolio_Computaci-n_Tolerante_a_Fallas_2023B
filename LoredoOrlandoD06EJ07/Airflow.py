426115f964489e2309ab6aac37f9bfc6f701d7de
Orlando-Javier-Loredo-Padilla <123122353+Orlando-Javier-Loredo-Padilla@users.noreply.github.com>
2023-10-15 19:54:27 -0600

    Add files via upload

---
+++ LoredoOrlandoD06EJ07/Airflow.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 16:58:51 2023
+
+@author: orlan
+"""
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+import datetime
+import pendulum
+
+dag = DAG(
+    "tutorial",
+    default_args={
+        "depends_on_past": True,
+        "retries": 1,
+        "retry_delay": datetime.timedelta(minutes=3),
+    },
+    start_date=pendulum.datetime(2015, 12, 1, tz="UTC"),
+    description="A simple tutorial DAG",
+    schedule="@daily",
+    catchup=False,
+)
